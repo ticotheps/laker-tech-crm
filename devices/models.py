b02fccd03f4c5f35ff2081f9a2c7c0bd4c854751
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-07-02 12:47:23 -0400

    feat(devices app): create 'Device' model

--- devices/models.py
+++ devices/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Device(models.Model):
+    manufacturer = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    serial_number = models.CharField(max_length=50)
+    imei_number = models.PositiveBigIntegerField()
+    wlan_mac_address = models.CharField(max_length=50)
+    lan_mac_address = models.CharField(max_length=50)
+    replacement_fee = models.DecimalField(max_digits=6, decimal_places=2)
+    
