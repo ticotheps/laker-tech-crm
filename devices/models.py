331475b48fb427c9f38d2c0d38035ab394a92d8c
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-07-05 09:18:31 -0400

    fix(models): update max number of chars for mac address entries

--- devices/models.py
+++ devices/models.py
@@ -5,7 +5,7 @@ class Device(models.Model):
     model = models.CharField(max_length=50)
     serial_number = models.CharField(max_length=50)
     imei_number = models.PositiveBigIntegerField()
-    wlan_mac_address = models.CharField(max_length=50)
-    lan_mac_address = models.CharField(max_length=50)
+    wlan_mac_address = models.CharField(max_length=17)
+    lan_mac_address = models.CharField(max_length=17)
     replacement_fee = models.DecimalField(max_digits=6, decimal_places=2)
     
