aa5843243596e7c8c33d31e49c6a5ea9e30d76a1
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-07-05 10:13:50 -0400

    feat(models): create dropdown choices for new 'device_type' field

--- devices/models.py
+++ devices/models.py
@@ -1,6 +1,31 @@
 from django.db import models
 
 class Device(models.Model):
+    DESKTOP     = 1
+    NOTEBOOK    = 2
+    TABLET      = 3
+    HOTSPOT     = 4
+    PROJECTOR   = 5
+    IP_PHONE    = 6
+    DOC_CAM     = 7
+    HD_TV       = 8
+    DRONE       = 9
+    TRI_POD     = 10
+    
+    DEVICE_TYPE = (
+        (DESKTOP, ('Desktop PC')),
+        (NOTEBOOK, ('Notebook PC')),
+        (TABLET, ('Tablet Device')),
+        (HOTSPOT, ('WiFi Hotspot')),
+        (PROJECTOR, ('Projector')),
+        (IP_PHONE, ('IP Phone')),
+        (DOC_CAM, ('Document Camera')),
+        (HD_TV, ('High-Def TV')),
+        (DRONE, ('Drone')),
+        (TRI_POD, ('Camera Tri-Pod')),
+    )
+    
+    device_type = models.PositiveSmallIntegerField(choices=DEVICE_TYPE, default=NOTEBOOK)
     manufacturer = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
     serial_number = models.CharField(max_length=50, verbose_name='Serial Number')
@@ -8,6 +33,13 @@ class Device(models.Model):
     wlan_mac_address = models.CharField(max_length=17, verbose_name='WLAN MAC Address')
     lan_mac_address = models.CharField(max_length=17, verbose_name='LAN MAC Address')
     replacement_fee = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Replacement Fee', default=0.00)
+    
+    def __str__(self):
+        return self.serial_number
+
+
+class DeviceType(models.Model):
+    name = models.CharField(max_length=50)
     
     def __str__(self):
         return self.name
