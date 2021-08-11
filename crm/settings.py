0ecc5d7c70f671c80b233968bf4a347636204585
Tico S. Thepsourinthone <ticotheps@gmail.com>
2021-08-11 11:04:27 -0400

    refactor(settings): remove variable declarations

--- crm/settings.py
+++ crm/settings.py
@@ -70,22 +70,15 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DEV_MODE = config('DEV_MODE', default=False, cast=bool)
 
-DB_NAME = config('DB_NAME')
-DB_USER = config('DB_USER')
-DB_PASSWORD = config('DB_PASSWORD')
-DB_HOST = config('DB_HOST')
-DB_PORT = config('DB_PORT')
-
-
 if DEV_MODE is True:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': DB_NAME,
-            'USER': DB_USER,
-            'PASSWORD': DB_PASSWORD,
-            'HOST': DB_HOST,
-            'PORT': DB_PORT
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT')
         }
     }
 elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
